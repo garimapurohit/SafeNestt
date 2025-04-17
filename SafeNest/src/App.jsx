b9b1a6f7093f94a83be02ade189e139b75c2ae6d
Shreya Jaiswal <shreyaa.we@gmail.com>
2025-04-18 00:51:19 +0530

    ..

--- SafeNest/src/App.jsx
+++ SafeNest/src/App.jsx
@@ -1,12 +1,24 @@
-
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Landing from "./components/Landing"
 
 function App() {
   return (
-    <>
-     
-    </>
-  )
+    <Router>
+      <div>
+         
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        
+
+      </Routes>
+    
+      </div>
+    </Router>
+
+  );
 }
+
 
 export default App
